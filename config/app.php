759d33c3e0083a6228d690164d67b9b64b9070fb
davykololi <kololimdavid@gmail.com>
2023-01-29 12:26:45 -0800

    Restored Renato

--- config/app.php
+++ config/app.php
@@ -175,7 +175,6 @@ return [
         Yoeunes\Toastr\ToastrServiceProvider::class,
         Spatie\Backup\BackupServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
-        Barryvdh\Debugbar\ServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -250,7 +249,6 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Image' => Intervention\Image\Facades\Image::class,
-        'Debugbar' => Barryvdh\Debugbar\Facade::class,
     ],
 
 ];
