60c0dd139e01b128a41f0ed4c2c92234ce91e416
davykololi <kololimdavid@gmail.com>
2023-01-29 12:12:11 -0800

    Removed Renato

--- config/app.php
+++ config/app.php
@@ -175,6 +175,7 @@ return [
         Yoeunes\Toastr\ToastrServiceProvider::class,
         Spatie\Backup\BackupServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -249,6 +250,7 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Image' => Intervention\Image\Facades\Image::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
     ],
 
 ];
