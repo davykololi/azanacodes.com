2f912fcbaa803e7887494965c35c124a0d2434e5
davykololi <kololimdavid@gmail.com>
2023-02-16 11:09:22 -0800

    Removed Script Files

--- resources/views/partials/adminlte_scripts.blade.php
+++ resources/views/partials/adminlte_scripts.blade.php
@@ -4,12 +4,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/dist/jquery/1.12.0/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 <!--Fontawesome script -->
-{{ Html::script('fontawesome5/js/fontawesome.min.js') }}
-<!--Bootstrap script -->
-{{ Html::script('bootstrap5/js/bootstrap.min.js') }} 
-<!--Bootstrap 5.0.2 CDN script -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous" defer></script>
-<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+{{ Html::script('fontawesome5/js/fontawesome.min.js') }} 
 <!-- Share on social media script -->
 {{ Html::script('js/share.js') }} 
 <!-- TinyMCE Scripts -->
